2b16f566e977126062aaa304c77b527a3dc16058
Calle226 <callaghan.elliot@yahoo.com>
2017-11-28 23:20:06 +0000

    Trying to get ajax to work

--- functions.js
+++ functions.js
@@ -7,10 +7,10 @@ $(document).on("scroll", function () {
 });
 
 $('a[href*="#"]')
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function (event) {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
@@ -30,3 +30,24 @@ $('a[href*="#"]')
             }
         }
     });
+
+$('form').submit(function (event) {
+    var formData = {
+        'name' : $('input[name=name]').val(),
+        'email' : $('input[name=email]').val(),
+        'subject' : $('input[name=subject]').val(),
+        'message' : $('input[name=message]').val()
+    };
+    
+    $.ajax({
+        type : 'POST',
+        url : 'submit.php',
+        data : formData,
+        dataType : 'json',
+        encode : true
+    })
+        .done(function (data) {
+            alert("success");
+        });
+    event.preventDefault();
+});
--- submit.php
+++ submit.php
@@ -1,33 +1,43 @@
+<!doctype html>
+<html>
+<head>
+    <title>Callaghan Development</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+</head>
+<body>
 <?php
-$name = $email = $subject = $message = '';
-$ip = $_SERVER['REMOTE_ADDR'];
+$name = $email = $subject = $message = "";
+$ip = $_SERVER["REMOTE_ADDR"];
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (empty($_POST['name']) {
-        echo 'Name is required';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        echo "Name is required";
     } else {
-        $name = $_POST['name'];
+        $name = $_POST["name"];
     }
 
-    if (empty($_POST['email']) {
-        echo 'Email is required';
-    } elseif (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-        echo 'Invalid email';
+    if (empty($_POST["email"])) {
+        echo "Email is required";
+    } elseif (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+        echo "Invalid email";
     } else {
-        $email = $_POST['email'];
+        $email = $_POST["email"];
     }
 
-    if (empty($_POST['message']) {
-        echo 'Message is required';
+    if (empty($_POST["message"])) {
+        echo "Message is required";
     } else {
-        $message = $_POST['message'];
+        $message = $_POST["message"];
     }
 
-    $hostname = 'db711614191.db.1and1.com';
-    $username = 'dbo711614191';
-    $password = 'Trappist-1';
-    $dbname = 'db711614191';
-    $port = '3306';
+    $hostname = "db711614191.db.1and1.com";
+    $username = "dbo711614191";
+    $password = "Trappist-1";
+    $dbname = "db711614191";
+    $port = "3306";
 
     $conn = new PDO("mysql:host=$hostname;dbname=$dbname;port=$port", $username, $password);
 
@@ -42,29 +52,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $conn = $stmt = null;
     
-    require 'PHPMailer-master/PHPMailer-master/src/Exception.php';
-    require 'PHPMailer-master/PHPMailer-master/src/PHPMailer.php';
-    require 'PHPMailer-master/PHPMailer-master/src/SMTP.php';
+    require "PHPMailer-master/PHPMailer-master/src/Exception.php";
+    require "PHPMailer-master/PHPMailer-master/src/PHPMailer.php";
+    require "PHPMailer-master/PHPMailer-master/src/SMTP.php";
 
     $mail = new PHPMailer\PHPMailer\PHPMailer;
         $mail->SMTPDebug = 1; //0 = nothing; 1 = error and messages; 2 = messages only
         $mail->isSMTP();
-        $mail->Host = 'auth.smtp.1and1.co.uk';
+        $mail->Host = "auth.smtp.1and1.co.uk";
         $mail->SMTPAuth = true;
-        $mail->Username = 'elliot@elliotcallaghan.co.uk';
-        $mail->Password = 'Trappist-1';
-        $mail->SMTPSecure = 'ssl';
+        $mail->Username = "elliot@elliotcallaghan.co.uk";
+        $mail->Password = "Trappist-1";
+        $mail->SMTPSecure = "ssl";
         $mail->Port = 587;
 
-        $mail->setFrom('.$email.', '.$name.');
-        $mail->addAddress('elliot@elliotcallaghan.co.uk');
+        $mail->setFrom("$email", "$name");
+        $mail->addAddress("elliot@elliotcallaghan.co.uk");
 
         $mail->isHTML(true);
-        $mail->Subject = '.$subject.';
-        $mail->Body = '.$message.';
+        $mail->Subject = "$subject";
+        $mail->Body = "$message";
 
         $mail->send();
 
-    echo 'Message sent.';
+    echo "Message sent.";
 }
 ?>
+</body>
+</html>
