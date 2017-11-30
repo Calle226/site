009bd80fbe29f904eee0930491c95e4ed827f352
Calle226 <callaghan.elliot@yahoo.com>
2017-11-30 17:58:49 +0000

    Made loads of changes that did basically nothing.

--- submit.php
+++ submit.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-    <title>Callaghan Development</title>
+    <title></title>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <link rel='stylesheet' type='text/css' href='style.css'>
@@ -9,11 +9,11 @@
 </head>
 <body>
 <?php
+$ip = $_SERVER['REMOTE_ADDR'];
 $name = $email = $message = '';
 $subject = $_POST['subject'];
-$ip = $_SERVER['REMOTE_ADDR']
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
     if (empty($_POST['name'])) {
         echo 'Name is required';
     } else {
@@ -34,23 +34,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $message = $_POST['message'];
     }
 
-    $hostname = 'db711614191.db.1and1.com';
-    $username = 'dbo711614191';
-    $password = '';
-    $dbname = 'db711614191';
-    $port = '3306';
+    $hostname = "db711614191.db.1and1.com";
+    $username = "dbo711614191";
+    $password = "";
+    $dbname = "db711614191";
+    $port = "3306";
 
-    $conn = new PDO('mysql:host=$hostname;dbname=$dbname;port=$port', $username, $password);
+    $conn = new PDO("mysql:host=$hostname;dbname=$dbname;port=$port", $username, $password);
 
-    $stmt = $conn->prepare('INSERT INTO submissions (name, email, ip) 
-    VALUES (:name, :email, :ip)');
+    $stmt = $conn->prepare("INSERT INTO submissions (name, email, ip)
+    VALUES(:name, :email, :ip)");
 
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':ip', $ip);
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":ip", $ip);
 
     $stmt->execute();
-
+    
     $conn = $stmt = null;
     
     require 'PHPMailer-master/PHPMailer-master/src/Exception.php';
@@ -58,24 +58,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     require 'PHPMailer-master/PHPMailer-master/src/SMTP.php';
 
     $mail = new PHPMailer\PHPMailer\PHPMailer;
-        $mail->SMTPDebug = 0; //0 = nothing; 1 = error and messages; 2 = messages only
-        $mail->isSMTP();
-        $mail->Host = 'auth.smtp.1and1.co.uk';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'elliot@elliotcallaghan.co.uk';
-        $mail->Password = '';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
+    $mail->SMTPDebug = 0; //0 = nothing; 1 = error and messages; 2 = messages only
+    $mail->isSMTP();
+    $mail->Host = 'auth.smtp.1and1.co.uk';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'elliot@elliotcallaghan.co.uk';
+    $mail->Password = '';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-        $mail->setFrom("$email", "$name");
-        $mail->addAddress("elliot@elliotcallaghan.co.uk");
+    $mail->setFrom("$email", "$name");
+    $mail->addAddress('elliot@elliotcallaghan.co.uk');
 
-        $mail->isHTML(true);
-        $mail->Subject = "$subject";
-        $mail->Body = "$message";
+    $mail->isHTML(true);
+    $mail->Subject = "$subject";
+    $mail->Body = "<body style='white-space: pre-wrap;'>$message</body>";
 
-        $mail->send();
-    
+    $mail->send();
 }
 ?>
 </body>
